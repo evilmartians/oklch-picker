7045229c91cc952cc47ab9ceb99a3d7709db63ae
Andrey Sitnik <andrey@sitnik.ru>
2022-02-22 04:02:47 +0100

    Speed up spaces generation

--- scripts/spaces-worker.js
+++ scripts/spaces-worker.js
@@ -15,33 +15,10 @@ async function every(from, to, cb) {
   }
 }
 
-async function writeSpace(name, xMax, yMax, getColors) {
+async function writeImage(name, cb) {
   let img = canvas.createCanvas(IMAGE_WIDTH, IMAGE_HEIGHT)
   let ctx = img.getContext('2d')
-
-  for (let x = 0; x <= IMAGE_WIDTH; x++) {
-    let xValue = (xMax * x) / IMAGE_WIDTH
-    let prevSRGB
-    for (let y = 0; y <= IMAGE_WIDTH; y++) {
-      let yValue = (yMax * y) / IMAGE_HEIGHT
-
-      let [l, c, h] = getColors(xValue, yValue)
-      let color = new Color('oklch', [l / 100, c / 100, h])
-
-      if (color.inGamut('p3')) {
-        let inSRGB = color.inGamut('srgb')
-        if (prevSRGB !== undefined && inSRGB !== prevSRGB) {
-          color = color.mix(l < L_MAX / 2 ? '#fff' : '#000')
-        }
-        ctx.fillStyle = toOldRgb(color.srgb)
-        ctx.fillRect(x, IMAGE_HEIGHT - y, 1, 1)
-        prevSRGB = inSRGB
-      } else {
-        prevSRGB = false
-      }
-    }
-  }
-
+  cb(ctx)
   await writeFile(join(BUILD, `${name}.webp`), img.toBuffer('image/webp'))
 }
 
@@ -54,15 +31,80 @@ async function build() {
   let to = parseInt(process.argv[4])
   if (process.argv[2] === 'l') {
     await every(from, to, async l => {
-      await writeSpace(`l-${l}`, H_MAX, C_MAX, (h, c) => [l, c, h])
+      let lMod = l / 100
+      let hFactor = H_MAX / IMAGE_WIDTH
+      let cFactor = C_MAX / IMAGE_HEIGHT / 100
+      await writeImage(`l-${l}`, ctx => {
+        for (let x = 0; x <= IMAGE_WIDTH; x++) {
+          let h = x * hFactor
+          let prevSRGB
+          for (let y = 0; y <= IMAGE_HEIGHT; y++) {
+            let color = new Color('oklch', [lMod, y * cFactor, h])
+
+            if (color.inGamut('p3')) {
+              let inSRGB = color.inGamut('srgb')
+              if (prevSRGB === undefined || inSRGB === prevSRGB) {
+                ctx.fillStyle = toOldRgb(color.srgb)
+                ctx.fillRect(x, IMAGE_HEIGHT - y, 1, 1)
+              }
+              prevSRGB = inSRGB
+            } else {
+              break
+            }
+          }
+        }
+      })
     })
   } else if (process.argv[2] === 'c') {
     await every(from, to, async c => {
-      await writeSpace(`c-${c}`, H_MAX, L_MAX, (h, l) => [l, c, h])
+      let cMod = c / 100
+      let hFactor = H_MAX / IMAGE_WIDTH
+      let lFactor = L_MAX / IMAGE_HEIGHT / 100
+      await writeImage(`c-${c}`, ctx => {
+        for (let x = 0; x <= IMAGE_WIDTH; x++) {
+          let h = x * hFactor
+          let prevSRGB
+          for (let y = 0; y <= IMAGE_HEIGHT; y++) {
+            let color = new Color('oklch', [y * lFactor, cMod, h])
+
+            if (color.inGamut('p3')) {
+              let inSRGB = color.inGamut('srgb')
+              if (prevSRGB === undefined || inSRGB === prevSRGB) {
+                ctx.fillStyle = toOldRgb(color.srgb)
+                ctx.fillRect(x, IMAGE_HEIGHT - y, 1, 1)
+              }
+              prevSRGB = inSRGB
+            } else {
+              prevSRGB = false
+            }
+          }
+        }
+      })
     })
   } else {
     await every(from, to, async h => {
-      await writeSpace(`h-${h}`, C_MAX, L_MAX, (c, l) => [l, c, h])
+      let cFactor = C_MAX / IMAGE_WIDTH / 100
+      let lFactor = L_MAX / IMAGE_HEIGHT / 100
+      await writeImage(`h-${h}`, ctx => {
+        for (let y = 0; y <= IMAGE_HEIGHT; y++) {
+          let l = y * lFactor
+          let prevSRGB
+          for (let x = 0; x <= IMAGE_WIDTH; x++) {
+            let color = new Color('oklch', [l, x * cFactor, h])
+
+            if (color.inGamut('p3')) {
+              let inSRGB = color.inGamut('srgb')
+              if (prevSRGB === undefined || inSRGB === prevSRGB) {
+                ctx.fillStyle = toOldRgb(color.srgb)
+                ctx.fillRect(x, IMAGE_HEIGHT - y, 1, 1)
+              }
+              prevSRGB = inSRGB
+            } else {
+              prevSRGB = false
+            }
+          }
+        }
+      })
     })
   }
 }
