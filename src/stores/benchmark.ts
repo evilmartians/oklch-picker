32b5592ca0478c5ca4455a037e9b189788ebdbaf
Andrey Sitnik <andrey@sitnik.ru>
2022-02-23 01:09:24 +0100

    Reduce size

--- src/stores/benchmark.ts
+++ src/stores/benchmark.ts
@@ -1,18 +1,6 @@
-import { atom, onMount } from 'nanostores'
+import { atom } from 'nanostores'
 
 export let benchmarking = atom(false)
-
-onMount(benchmarking, () => {
-  function keyUp(e: KeyboardEvent): void {
-    if (e.key === 'b' && e.target === document.body) {
-      benchmarking.set(!benchmarking.get())
-    }
-  }
-  document.body.addEventListener('keyup', keyUp)
-  return () => {
-    document.body.removeEventListener('keyup', keyUp)
-  }
-})
 
 export let lastBenchmark = atom(0)
 
@@ -21,3 +9,10 @@ export function reportBenchmark(ms: number): void {
     lastBenchmark.set(ms)
   }
 }
+
+function keyUp(e: KeyboardEvent): void {
+  if (e.key === 'b' && e.target === document.body) {
+    benchmarking.set(!benchmarking.get())
+  }
+}
+document.body.addEventListener('keyup', keyUp)
