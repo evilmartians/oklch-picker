3faf0f6da0c0858197b01c6f62ad37f985f0033c
Andrey Sitnik <andrey@sitnik.ru>
2022-04-29 11:58:18 +0200

    Temporary disable web worker to fix Chrome

--- src/spaces/index.ts
+++ src/spaces/index.ts
@@ -1,8 +1,6 @@
-import type { MessageData } from './worker.js'
-
 import './index.css'
-import { reportPaint, bindFreezeToPaint } from '../stores/benchmark.js'
-import { pixelRation, hasP3Support } from '../../lib/screen.js'
+import { bindFreezeToPaint } from '../stores/benchmark.js'
+import { pixelRation } from '../../lib/screen.js'
 import { paintL, paintC, paintH } from './lib.js'
 import { L_MAX, C_MAX, H_MAX } from '../../config.js'
 import { onCurrentChange } from '../stores/current.js'
@@ -36,58 +34,15 @@ onCurrentChange({
   }
 })
 
-if (canvasL.transferControlToOffscreen) {
-  function send(worker: Worker, message: MessageData): void {
-    if (message.type === 'init') {
-      worker.postMessage(message, [message.canvas])
-    } else {
-      worker.postMessage(message)
-    }
+bindFreezeToPaint()
+onCurrentChange({
+  l(l) {
+    paintL(canvasL, WIDTH, HEIGHT, l)
+  },
+  c(c) {
+    paintC(canvasC, WIDTH, HEIGHT, c)
+  },
+  h(h) {
+    paintH(canvasH, WIDTH, HEIGHT, h)
   }
-
-  let workerUrl = new URL('./worker.ts', import.meta.url)
-
-  function init(canvas: HTMLCanvasElement): Worker {
-    let worker = new Worker(workerUrl, { type: 'module' })
-    send(worker, {
-      type: 'init',
-      canvas: canvas.transferControlToOffscreen!(),
-      width: WIDTH,
-      height: HEIGHT,
-      p3: hasP3Support ?? false
-    })
-    worker.onmessage = (e: MessageEvent<number>) => {
-      reportPaint(e.data)
-    }
-    return worker
-  }
-
-  let workerL = init(canvasL)
-  let workerC = init(canvasC)
-  let workerH = init(canvasH)
-
-  onCurrentChange({
-    l(l) {
-      send(workerL, { type: 'l', l })
-    },
-    c(c) {
-      send(workerC, { type: 'c', c })
-    },
-    h(h) {
-      send(workerH, { type: 'h', h })
-    }
-  })
-} else {
-  bindFreezeToPaint()
-  onCurrentChange({
-    l(l) {
-      paintL(canvasL, WIDTH, HEIGHT, l)
-    },
-    c(c) {
-      paintC(canvasC, WIDTH, HEIGHT, c)
-    },
-    h(h) {
-      paintH(canvasH, WIDTH, HEIGHT, h)
-    }
-  })
-}
+})
