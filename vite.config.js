72e6efafa7a214855b1732580246f1160702b7d8
Andrey Sitnik <andrey@sitnik.ru>
2022-05-02 08:32:37 +0200

    Move block templates to Pug

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import vitePluginPug from 'vite-plugin-pug-transformer'
 
 export default defineConfig({
   build: {
     assetsDir: '.'
-  }
+  },
+  plugins: [vitePluginPug()]
 })
