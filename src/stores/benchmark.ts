eecd6e51e1f820edcaa901c36b73fca8c5779116
Andrey Sitnik <andrey@sitnik.ru>
2022-05-03 16:50:33 +0200

    Use ?bench to start benchmark

--- src/stores/benchmark.ts
+++ src/stores/benchmark.ts
@@ -2,6 +2,20 @@ import { atom, map } from 'nanostores'
 
 export let benchmarking = atom(false)
 
+if (location.search === '?bench') {
+  benchmarking.set(true)
+}
+benchmarking.listen(value => {
+  history.pushState(null, '', location.pathname + (value ? '?bench' : ''))
+})
+
+function keyUp(e: KeyboardEvent): void {
+  if (e.key === 'b' && e.target === document.body) {
+    benchmarking.set(!benchmarking.get())
+  }
+}
+document.body.addEventListener('keyup', keyUp)
+
 export let lastBenchmark = map({ freeze: 0, paint: 0 })
 
 let bound = false
@@ -9,9 +23,14 @@ export function bindFreezeToPaint(): void {
   bound = true
 }
 
+let initializing = true
+let initFreeze = 0
+let initPaint = 0
 export function reportFreeze(ms: number): void {
   if (benchmarking.get()) {
-    if (bound) {
+    if (initializing) {
+      initFreeze += ms
+    } else if (bound) {
       lastBenchmark.set({ freeze: ms, paint: ms })
     } else {
       lastBenchmark.setKey('freeze', ms)
@@ -21,13 +40,21 @@ export function reportFreeze(ms: number): void {
 
 export function reportPaint(ms: number): void {
   if (benchmarking.get()) {
-    lastBenchmark.setKey('paint', ms)
+    if (initializing) {
+      initPaint += ms
+    } else {
+      lastBenchmark.setKey('paint', ms)
+    }
   }
 }
 
-function keyUp(e: KeyboardEvent): void {
-  if (e.key === 'b' && e.target === document.body) {
-    benchmarking.set(!benchmarking.get())
+setTimeout(() => {
+  initializing = false
+  if (initPaint + initFreeze > 0) {
+    if (bound) {
+      lastBenchmark.set({ freeze: initFreeze, paint: initFreeze })
+    } else {
+      lastBenchmark.set({ freeze: initFreeze, paint: initPaint })
+    }
   }
-}
-document.body.addEventListener('keyup', keyUp)
+}, 1)
