6bac29dbe997e81ffe5be570db36fa40ef08ef8b
Andrey Sitnik <andrey@sitnik.ru>
2022-05-04 13:55:40 +0200

    Update sample design

--- view/sample/index.ts
+++ view/sample/index.ts
@@ -2,13 +2,14 @@ import './index.css'
 import { visible } from '../../stores/visible.js'
 
 let sample = document.querySelector<HTMLDivElement>('.sample')!
-let spaceNote = sample.querySelector<HTMLDivElement>('.sample_note.is-space')!
 
 visible.subscribe(({ space, real, fallback }) => {
   sample.classList.toggle('is-srgb', space === 'srgb')
+  sample.classList.toggle('is-p3', space === 'p3')
+  sample.classList.toggle('is-rec2020', space === 'rec2020')
   sample.classList.toggle('is-out', space === 'out')
   sample.classList.toggle('is-supported', !!real)
+
   sample.style.setProperty('--sample-real', real || 'transparent')
   sample.style.setProperty('--sample-fallback', fallback)
-  spaceNote.innerText = space === 'p3' || space === 'rec2020' ? space : ''
 })
