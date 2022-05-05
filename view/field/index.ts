9d8ffeac59b7c31dd7da34ed5484ff405d82893c
Andrey Sitnik <andrey@sitnik.ru>
2022-05-05 13:48:06 +0200

    Add custom text fields

--- view/field/index.ts
+++ view/field/index.ts
@@ -1,1 +1,12 @@
 import './index.css'
+
+let fields = document.querySelectorAll<HTMLInputElement>('.field')
+
+function onFocus(e: FocusEvent): void {
+  let input = e.target as HTMLInputElement
+  input.select()
+}
+
+for (let field of fields) {
+  field.addEventListener('focus', onFocus)
+}
