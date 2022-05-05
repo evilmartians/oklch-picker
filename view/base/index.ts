4f4151d9c1583a2c2174d069e88cb08286e05d44
Andrey Sitnik <andrey@sitnik.ru>
2022-05-05 17:56:31 +0200

    Use accent in theme_color

--- view/base/index.ts
+++ view/base/index.ts
@@ -2,7 +2,10 @@ import './reset.css'
 import './index.css'
 import { accent } from '../../stores/accent.js'
 
+let theme = document.querySelector<HTMLMetaElement>('meta[name=theme-color]')!
+
 accent.subscribe(({ main, surface }) => {
   document.body.style.setProperty('--accent', main)
   document.body.style.setProperty('--surface-ui-accent', surface)
+  theme.content = main
 })
