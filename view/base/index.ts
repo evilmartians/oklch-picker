97517a89e878633206c6584be04bc3b799c32d5c
Andrey Sitnik <andrey@sitnik.ru>
2022-05-08 15:33:10 +0200

    Fix logo hover

--- view/base/index.ts
+++ view/base/index.ts
@@ -4,10 +4,9 @@ import { accent } from '../../stores/accent.js'
 
 let theme = document.querySelector<HTMLMetaElement>('meta[name=theme-color]')!
 
-accent.subscribe(({ main, surface, martian }) => {
+accent.subscribe(({ main, surface }) => {
   document.body.style.setProperty('--accent', main)
   document.body.style.setProperty('--surface-ui-accent', surface)
-  document.body.style.setProperty('--martian', martian)
   theme.content = main
 })
 
