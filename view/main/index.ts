9196628861aeba29e838840e37ea0384db4806d0
Andrey Sitnik <andrey@sitnik.ru>
2022-05-08 17:01:16 +0200

    Connect text and logo links

--- view/main/index.ts
+++ view/main/index.ts
@@ -2,6 +2,9 @@ import './index.css'
 
 let expand = document.querySelector<HTMLButtonElement>('.main_expand')!
 
+let emText = document.querySelector<HTMLLinkElement>('.main_link.is-em')!
+let emLogo = document.querySelector<HTMLLinkElement>('.main_logo')!
+
 function onScroll(): void {
   document.body.classList.add('is-main-collapsed')
   window.removeEventListener('scroll', onScroll)
@@ -22,4 +25,20 @@ if (mobile.matches) {
 
 expand.addEventListener('click', () => {
   document.body.classList.toggle('is-main-collapsed')
+})
+
+emText.addEventListener('mouseenter', () => {
+  emLogo.classList.add('is-hover')
+})
+
+emText.addEventListener('mouseleave', () => {
+  emLogo.classList.remove('is-hover')
+})
+
+emLogo.addEventListener('mouseenter', () => {
+  emText.classList.add('is-hover')
+})
+
+emLogo.addEventListener('mouseleave', () => {
+  emText.classList.remove('is-hover')
 })
