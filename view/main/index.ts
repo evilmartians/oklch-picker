d9c78ed20bdb9f33ea48c2692f07df9ffd76ed23
Andrey Sitnik <andrey@sitnik.ru>
2022-05-08 13:55:58 +0200

    Hide/show main panel on mobile

--- view/main/index.ts
+++ view/main/index.ts
@@ -1,1 +1,26 @@
 import './index.css'
+
+let main = document.querySelector<HTMLDivElement>('.main')!
+let expand = document.querySelector<HTMLButtonElement>('.main_expand')!
+
+function onScroll(): void {
+  main.classList.add('is-collapsed')
+  window.removeEventListener('scroll', onScroll)
+}
+
+function init(): void {
+  window.addEventListener('scroll', onScroll)
+}
+
+let mobile = window.matchMedia('(max-width: 908px)')
+if (mobile.matches) {
+  init()
+} else {
+  mobile.addEventListener('change', () => {
+    init()
+  })
+}
+
+expand.addEventListener('click', () => {
+  main.classList.toggle('is-collapsed')
+})
