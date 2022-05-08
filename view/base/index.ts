19dc1a9edd6b1ecf9c40891ed991078dd22cb216
Andrey Sitnik <andrey@sitnik.ru>
2022-05-08 16:46:05 +0200

    Do not use dynamic theme color

--- view/base/index.ts
+++ view/base/index.ts
@@ -2,12 +2,9 @@ import './reset.css'
 import './index.css'
 import { accent } from '../../stores/accent.js'
 
-let theme = document.querySelector<HTMLMetaElement>('meta[name=theme-color]')!
-
 accent.subscribe(({ main, surface }) => {
   document.body.style.setProperty('--accent', main)
   document.body.style.setProperty('--surface-ui-accent', surface)
-  theme.content = main
 })
 
 setTimeout(() => {
