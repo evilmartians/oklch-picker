cc1f1f25db1f0b7f5cf87e61daad4556eee2227d
Andrey Sitnik <andrey@sitnik.ru>
2022-05-09 20:40:14 +0200

    Add logo

--- stores/support.ts
+++ stores/support.ts
@@ -4,7 +4,7 @@ export let support = atom(false)
 
 if (typeof window !== 'undefined') {
   if (CSS.supports('color', 'color(display-p3 0 0 0)')) {
-    let media = window.matchMedia('(color-gamut: p3)')
+    let media = window.matchMedia('(color-gamut:p3)')
     support.set(media.matches)
     media.addEventListener('change', () => {
       support.set(media.matches)
