3a3e334702942879c17cc9808eff533976b7a92c
Andrey Sitnik <andrey@sitnik.ru>
2022-05-25 00:13:05 +0200

    Read current color space

--- view/sample/index.ts
+++ view/sample/index.ts
@@ -2,6 +2,7 @@ import './index.css'
 import { visible } from '../../stores/visible.js'
 
 let sample = document.querySelector<HTMLDivElement>('.sample')!
+let type = document.querySelector<HTMLDivElement>('.sample_reader')!
 
 visible.subscribe(({ space, real, fallback }) => {
   sample.classList.toggle('is-srgb', space === 'srgb')
@@ -9,6 +10,8 @@ visible.subscribe(({ space, real, fallback }) => {
   sample.classList.toggle('is-rec2020', space === 'rec2020')
   sample.classList.toggle('is-out', space === 'out')
   sample.classList.toggle('is-supported', !!real)
+
+  type.innerText = `Color space ${space}`
 
   sample.style.setProperty('--sample-real', real || 'transparent')
   sample.style.setProperty('--sample-fallback', fallback)
