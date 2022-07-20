4fff735ad011bb3daa7574de787f17ea5a79aa2a
Andrey Sitnik <andrey@sitnik.ru>
2022-07-20 17:48:47 +0200

    Fix accent colors for LCH

--- stores/accent.ts
+++ stores/accent.ts
@@ -1,19 +1,24 @@
-import { Oklch } from 'culori/fn'
 import { computed } from 'nanostores'
 
-import { toRgb, formatRgb } from '../lib/colors.js'
+import { toRgb, formatRgb, build } from '../lib/colors.js'
 import { current } from './current.js'
 
 function clamp(l: number, c: number, h: number, alpha = 1): string {
-  let lch: Oklch = { mode: 'oklch', l, c, h, alpha }
-  return formatRgb(toRgb(lch))
+  return formatRgb(toRgb(build(l, c, h, alpha)))
 }
 
 export let accent = computed(current, value => {
   let { h } = value
   if (h === 0) h = 286
-  return {
-    main: clamp(0.57, 0.18, h),
-    surface: clamp(0.7, 0.17, h, 0.2)
+  if (COLOR_FN === 'oklch') {
+    return {
+      main: clamp(0.57, 0.18, h),
+      surface: clamp(0.7, 0.17, h, 0.2)
+    }
+  } else {
+    return {
+      main: clamp(47, 58, h),
+      surface: clamp(63, 55, h, 0.2)
+    }
   }
 })
