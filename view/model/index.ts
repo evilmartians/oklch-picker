aadac38178a0a2433468c330e7bfaf2e159eb35a
Andrey Sitnik <andrey@sitnik.ru>
2023-03-10 22:10:34 +0100

    Fix LCH model

--- view/model/index.ts
+++ view/model/index.ts
@@ -5,8 +5,10 @@ import { mode } from '../../stores/mode.js'
 
 let button = document.querySelector<HTMLButtonElement>('[data-button=model]')!
 let model = document.querySelector<HTMLDivElement>('.model')!
+let title = document.querySelector<HTMLHeadingElement>('.model_title')!
 
 let showText = button.innerText
+let titleText = title.innerText
 
 button.addEventListener('click', () => {
   mode.setKey('model', !mode.get().model)
@@ -19,8 +21,10 @@ async function init(): Promise<void> {
   if (initialized) return
   initialized = true
 
+  title.innerText = 'Loading…'
   let { generate3d } = await import('./model.js')
   if (!loading) await delay(400)
+  title.innerText = titleText
 
   generate3d(showP3.get(), showRec2020.get())
   showP3.listen(() => {
