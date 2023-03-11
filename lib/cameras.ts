ada456c996241eb31303fbee7f0c07312fb583f9
Andrey Sitnik <andrey@sitnik.ru>
2023-03-11 23:05:49 +0100

    Allow zoom at full model

--- lib/cameras.ts
+++ lib/cameras.ts
@@ -1,17 +1,28 @@
-import type { Camera } from 'three'
+import type { Camera, Vector3 } from 'three'
 
 let cameras = new Map<string, Camera>()
 
+let last: Camera | undefined
+
+function sync(camera: Camera, position: Vector3): void {
+  let distance = camera.position.distanceTo(position.clone().set(0, 0, 0))
+  camera.position.copy(position)
+  camera.position.normalize().multiplyScalar(distance)
+}
+
 export function registerCamera(camera: Camera, id: string): void {
   cameras.set(id, camera)
+  if (last) sync(camera, last.position)
 }
 
 export function syncCamerasFrom(sourceId: string): void {
-  let source = cameras.get(sourceId)?.position
-  if (source) {
+  let sourceCamera = cameras.get(sourceId)
+  if (sourceCamera) {
+    last = sourceCamera
+    let source = sourceCamera.position
     for (let [id, camera] of cameras.entries()) {
       if (id !== sourceId) {
-        camera.position.set(source.x, source.y, source.z)
+        sync(camera, source)
       }
     }
   }
