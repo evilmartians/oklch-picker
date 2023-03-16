e3586aa99de295d160fab31de8c0e5d09c39b477
Andrey Sitnik <andrey@sitnik.ru>
2023-03-17 00:00:24 +0100

    Move unsupport text to JS from CSS

--- view/sample/index.ts
+++ view/sample/index.ts
@@ -2,13 +2,21 @@ import { visible } from '../../stores/visible.js'
 
 let sample = document.querySelector<HTMLDivElement>('.sample')!
 let type = document.querySelector<HTMLDivElement>('.sample_reader')!
+let unavailable = document.querySelector<HTMLDivElement>('.sample_unavailable')!
 
 visible.subscribe(({ space, real, fallback }) => {
   sample.classList.toggle('is-srgb', space === 'srgb')
-  sample.classList.toggle('is-p3', space === 'p3')
-  sample.classList.toggle('is-rec2020', space === 'rec2020')
-  sample.classList.toggle('is-out', space === 'out')
   sample.classList.toggle('is-supported', !!real)
+
+  if (real) {
+    unavailable.innerText = ''
+  } else if (space === 'p3') {
+    unavailable.innerText = 'P3 is unavailable on this device'
+  } else if (space === 'rec2020') {
+    unavailable.innerText = 'Rec2020 is unavailable on this device'
+  } else if (space === 'out') {
+    unavailable.innerText = 'Unavailable on any device'
+  }
 
   type.innerText = `${space} space`
 
