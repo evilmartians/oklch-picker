08c31a572370fd12afcfe1e2d694c83a05d5fa6c
Andrey Sitnik <andrey@sitnik.ru>
2023-05-15 08:42:42 +0200

    Add hack to FF

--- stores/support.ts
+++ stores/support.ts
@@ -11,7 +11,10 @@ export let support = map<SupportValue>({
 })
 
 if (typeof window !== 'undefined') {
-  if (CSS.supports('color', 'color(display-p3 1 1 1)')) {
+  // FF can’t display P3 yet. Remove when this issue will be resolved.
+  // https://bugzilla.mozilla.org/show_bug.cgi?id=1626624
+  let isFF = navigator.userAgent.toLowerCase().includes('firefox')
+  if (CSS.supports('color', 'color(display-p3 1 1 1)') && !isFF) {
     let media = window.matchMedia('(color-gamut:p3)')
     support.set({ p3: media.matches, oklch: true })
     media.addEventListener('change', () => {
