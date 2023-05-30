d2899f5734118ceb15ac7af0f6f426b7f8fb5169
Andrey Sitnik <andrey@sitnik.ru>
2023-05-30 09:23:32 +0200

    Clean up code

--- lib/math.ts
+++ lib/math.ts
@@ -2,17 +2,17 @@ enum Priority {
   Low = 1,
   High = 2
 }
-let DefaultValue = 0
 
-type Operators = typeof operators
-let operators = {
+const OPERATORS = {
   '+': Priority.Low,
   '-': Priority.Low,
   '*': Priority.High,
   '/': Priority.High
 }
 
-let operations = {
+type Operator = keyof typeof OPERATORS
+
+const OPERATIONS = {
   '+': (a: number, b: number) => a + b,
   '-': (a: number, b: number) => b - a,
   '*': (a: number, b: number) => a * b,
@@ -22,7 +22,11 @@ let operations = {
 export function parseValue(n: number | string): number {
   if (typeof n === 'number') return n
   let parsedValue = parseFloat(n)
-  return isNaN(parsedValue) ? DefaultValue : parsedValue
+  return isNaN(parsedValue) ? 0 : parsedValue
+}
+
+function checkPriority(a: Operator, b: Operator): boolean {
+  return OPERATORS[a] <= OPERATORS[b]
 }
 
 function toPostfix(str: string): string[] {
@@ -32,19 +36,16 @@ function toPostfix(str: string): string[] {
   for (let i = 0; i < str.length; i++) {
     let current = str.charAt(i)
 
-    if (current in operators) {
+    if (current in OPERATORS) {
       if (stack.length === 0) {
         stack.push(current)
         continue
       }
 
-      let topElement = stack[stack.length - 1] as keyof Operators
-      let currentPicked = current as keyof Operators
+      let topElement = stack[stack.length - 1] as Operator
+      let currentPicked = current as Operator
 
-      while (
-        stack.length > 0 &&
-        operators[topElement] <= operators[currentPicked]
-      ) {
+      while (stack.length > 0 && checkPriority(topElement, currentPicked)) {
         result.push(stack.pop()!)
       }
       stack.push(current)
@@ -74,15 +75,15 @@ export function computeExpression(str: string): number {
   let stack: number[] = []
 
   for (let ch of postfixOfExpression) {
-    if (ch in operations) {
-      let callback = operations[ch as keyof Operators]
+    if (ch in OPERATIONS) {
+      let callback = OPERATIONS[ch as Operator]
       stack.push(callback(stack.pop()!, stack.pop()!))
     } else {
       stack.push(parseValue(ch))
     }
   }
 
-  let result = stack.pop() ?? DefaultValue
+  let result = stack.pop() ?? 0
 
   return result
 }
