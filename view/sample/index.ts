a076fb164e40a70afae50166d546650279309b98
Andrey Sitnik <andrey@sitnik.ru>
2023-09-26 01:08:51 +0200

    Move to fallback color on fallback note click

--- view/sample/index.ts
+++ view/sample/index.ts
@@ -1,9 +1,14 @@
+import { lch, oklch } from '../../lib/colors.js'
+import { colorToValue, current } from '../../stores/current.js'
 import { support } from '../../stores/support.js'
 import { visible } from '../../stores/visible.js'
 
 let sample = document.querySelector<HTMLDivElement>('.sample')!
 let type = document.querySelector<HTMLDivElement>('.sample_reader')!
 let unavailable = document.querySelector<HTMLDivElement>('.sample_unavailable')!
+let fallbackNote = document.querySelector<HTMLButtonElement>(
+  '.sample_fallback .sample_note'
+)!
 
 let postfix = support.get().oklch ? ' on this monitor' : ' in this browser'
 
@@ -25,4 +30,10 @@ visible.subscribe(({ fallback, real, space }) => {
 
   sample.style.setProperty('--sample-real', real || 'transparent')
   sample.style.setProperty('--sample-fallback', fallback)
+})
+
+fallbackNote.addEventListener('click', () => {
+  let fallback = visible.get().fallback
+  let color = COLOR_FN === 'lch' ? lch(fallback) : oklch(fallback)
+  current.set(colorToValue(color!))
 })
