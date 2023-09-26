7551e7f75d958213e0a0fa2e05d4854fe1fb2789
Andrey Sitnik <andrey@sitnik.ru>
2023-09-26 21:16:55 +0200

    Organize config

--- config.js
+++ config.js
@@ -1,31 +1,34 @@
 let config = {
   ALPHA_MAX: 100,
+  ALPHA_STEP: 1,
 
-  ALPHA_STEP: 5,
   C_MAX: 0.37,
-
   C_MAX_REC2020: 0.47,
   C_RANDOM: 0.1,
   C_STEP: 0.01,
+
   COLOR_FN: '"oklch"',
 
   GAMUT_EPSILON: 1e-6,
+
   H_MAX: 360,
-
   H_STEP: 1,
-  L_MAX: 1,
 
+  L_MAX: 1,
   L_STEP: 1
 }
 
 if (process.env.LCH) {
   config = {
     ...config,
+
     C_MAX: 145,
     C_MAX_REC2020: 195,
     C_RANDOM: 39,
     C_STEP: 1,
+
     COLOR_FN: '"lch"',
+
     L_MAX: 100
   }
 }
