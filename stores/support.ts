b834c955a613c0df9d976c3e7f839df436e1bca0
Andrey Sitnik <andrey@sitnik.ru>
2024-01-17 18:58:22 +0100

    Remove FF check

--- stores/support.ts
+++ stores/support.ts
@@ -11,10 +11,7 @@ export let support = map<SupportValue>({
 })
 
 if (typeof window !== 'undefined') {
-  // FF can’t display P3 yet. Remove when this issue will be resolved.
-  // https://bugzilla.mozilla.org/show_bug.cgi?id=1626624
-  let isFF = navigator.userAgent.toLowerCase().includes('firefox')
-  if (CSS.supports('color', 'color(display-p3 1 1 1)') && !isFF) {
+  if (CSS.supports('color', 'color(display-p3 1 1 1)')) {
     let media = window.matchMedia('(color-gamut:p3)')
     support.set({ oklch: true, p3: media.matches })
     media.addEventListener('change', () => {
