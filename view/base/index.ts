dffcabf97af960c49cd1c9eb44a782bcd41c22d6
Sasha Mansurov <sasha@mansurov.me>
2024-07-20 18:19:15 +0200

    Uppercase constants

--- view/base/index.ts
+++ view/base/index.ts
@@ -4,8 +4,8 @@ import { redo, undo } from '../../stores/history.js'
 import { loading } from '../../stores/loading.js'
 
 const IS_MAC = /Mac|iPod|iPhone|iPad/.test(window.navigator.platform)
-const redoHotkeys = IS_MAC ? ['meta+shift+z'] : ['ctrl+shift+z', 'ctrl+y']
-const undoHotkeys = IS_MAC ? ['meta+z'] : ['ctrl+z']
+const REDO_HOTKEYS = IS_MAC ? ['meta+shift+z'] : ['ctrl+shift+z', 'ctrl+y']
+const UNDO_HOTKEYS = IS_MAC ? ['meta+z'] : ['ctrl+z']
 
 accent.subscribe(({ main, surface }) => {
   document.body.style.setProperty('--accent', main)
@@ -25,10 +25,10 @@ document.body.addEventListener('mouseup', () => {
 })
 
 document.body.addEventListener('keydown', e => {
-  if (isHotkey(redoHotkeys, e)) {
+  if (isHotkey(REDO_HOTKEYS, e)) {
     e.preventDefault()
     redo()
-  } else if (isHotkey(undoHotkeys, e)) {
+  } else if (isHotkey(UNDO_HOTKEYS, e)) {
     e.preventDefault()
     undo()
   }
