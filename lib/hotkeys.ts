71f3b5e6e64376fe50a34e61fefe904407a3dd64
Andrey Sitnik <andrey@sitnik.ru>
2024-07-22 16:01:13 +0000

    Do not do the same job again in the loop

--- lib/hotkeys.ts
+++ lib/hotkeys.ts
@@ -7,18 +7,13 @@ export function convertKey(e: KeyboardEvent): string {
   return e.key.toLowerCase()
 }
 
-function compareHotkey(hotkey: string, e: KeyboardEvent): boolean {
+export function isHotkey(hotkeys: string[], e: KeyboardEvent): boolean {
   let prefix = ''
   if (e.metaKey) prefix += 'meta+'
   if (e.ctrlKey) prefix += 'ctrl+'
   if (e.altKey) prefix += 'alt+'
   if (e.shiftKey) prefix += 'shift+'
-
   let code = prefix + convertKey(e)
 
-  return code === hotkey
-}
-
-export function isHotkey(hotkeys: string[], e: KeyboardEvent): boolean {
-  return hotkeys.some(k => compareHotkey(k, e))
+  return hotkeys.some(hotkey => code === hotkey)
 }
