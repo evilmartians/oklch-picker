4386766f7d4fb7a92e856f8e3f8d54ffc8f7817f
baileys-li <47776594+baileys-li@users.noreply.github.com>
2024-08-08 14:07:31 -0400

    Sync hover of logo and em link hovers with CSS only (#157)
    * ♻️ Reuse same logic for listeners
    
    * Sync hover of logo and em link hovers without js
    
    * Correct type for `a` tag
    
    * Rename `.is-em` to BEM variant
    
    * Use `.is-em` again with class combination

--- view/main/index.ts
+++ view/main/index.ts
@@ -2,10 +2,7 @@ import { trackEvent } from '../analytics/index.js'
 
 let expand = document.querySelector<HTMLButtonElement>('.main_expand')!
 
-let emText = document.querySelector<HTMLLinkElement>('.main_link.is-em')!
-let emLogo = document.querySelector<HTMLLinkElement>('.main_logo')!
-
-let links = document.querySelectorAll<HTMLLinkElement>('.main a')
+let links = document.querySelectorAll<HTMLAnchorElement>('.main a')
 
 function onScroll(): void {
   document.body.classList.add('is-main-collapsed')
@@ -27,30 +24,6 @@ if (mobile.matches) {
 
 expand.addEventListener('click', () => {
   document.body.classList.toggle('is-main-collapsed')
-})
-
-emText.addEventListener('mouseenter', () => {
-  emLogo.classList.add('is-hover')
-})
-
-emText.addEventListener('mouseleave', () => {
-  emLogo.classList.remove('is-hover')
-})
-
-emText.addEventListener('focus', () => {
-  emLogo.classList.add('is-hover')
-})
-
-emText.addEventListener('blur', () => {
-  emLogo.classList.remove('is-hover')
-})
-
-emLogo.addEventListener('mouseenter', () => {
-  emText.classList.add('is-hover')
-})
-
-emLogo.addEventListener('mouseleave', () => {
-  emText.classList.remove('is-hover')
 })
 
 for (let link of links) {
