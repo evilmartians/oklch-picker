d0abe9f1e2c125a1258e5375219015ab6c86df88
Andrey Sitnik <andrey@sitnik.ru>
2025-03-18 23:35:10 +0000

    Add tests for non-standard input

---
+++ test/inputs.test.ts
@@ -0,0 +1,22 @@
+import './set-globals.js'
+
+import { deepStrictEqual } from 'node:assert'
+import { test } from 'node:test'
+
+import { current, setCurrent } from '../stores/current.ts'
+
+test('understand CSS declaration', () => {
+  setCurrent('color: oklch(80% 0.1 195);')
+  deepStrictEqual(current.get(), { a: 100, c: 0.1, h: 195, l: 80 })
+
+  setCurrent('color: #f00')
+  deepStrictEqual(current.get(), { a: 100, c: 0.2577, h: 29.23, l: 62.8 })
+})
+
+test('understand hex without #', () => {
+  setCurrent('f00')
+  deepStrictEqual(current.get(), { a: 100, c: 0.2577, h: 29.23, l: 62.8 })
+
+  setCurrent('ff0000')
+  deepStrictEqual(current.get(), { a: 100, c: 0.2577, h: 29.23, l: 62.8 })
+})
