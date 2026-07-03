91d2e29d60292161287d7afe460d73373ce9d2b2
zmm2025 <zmm2025@gmail.com>
2026-07-03 17:10:45 -0400

    Support bare hex color input

--- test/inputs.test.ts
+++ test/inputs.test.ts
@@ -1,30 +1,43 @@
 import './set-globals.ts'
 
-import { deepStrictEqual } from 'node:assert'
+import { deepStrictEqual, strictEqual } from 'node:assert'
 import { test } from 'node:test'
 
 import { current, setCurrent } from '../stores/current.ts'
 
 test('understands CSS declaration', () => {
-  setCurrent('color: oklch(80% 0.1 195);')
+  strictEqual(setCurrent('color: oklch(80% 0.1 195);'), true)
   deepStrictEqual(current.get(), { a: 100, c: 0.1, h: 195, l: 0.8 })
 
-  setCurrent('color: #f00')
+  strictEqual(setCurrent('color: #f00'), true)
   deepStrictEqual(current.get(), { a: 100, c: 0.2577, h: 29.23, l: 0.628 })
 })
 
 test('understands hex without #', () => {
-  setCurrent('f00')
-  deepStrictEqual(current.get(), { a: 100, c: 0.2577, h: 29.23, l: 0.628 })
+  for (let [bare, prefixed] of [
+    ['fff', '#fff'],
+    ['ffff', '#ffff'],
+    ['60a7d6', '#60a7d6'],
+    ['60a7d680', '#60a7d680']
+  ]) {
+    strictEqual(setCurrent(prefixed), true)
+    let expected = current.get()
 
-  setCurrent('ff0000')
-  deepStrictEqual(current.get(), { a: 100, c: 0.2577, h: 29.23, l: 0.628 })
+    strictEqual(setCurrent(bare), true)
+    deepStrictEqual(current.get(), expected)
+  }
+})
+
+test('rejects invalid hex without #', () => {
+  for (let input of ['ff', 'fffff', 'zzzzzz']) {
+    strictEqual(setCurrent(input), false)
+  }
 })
 
 test('understands OKLCH without function', () => {
-  setCurrent('80% 0.1 195')
+  strictEqual(setCurrent('80% 0.1 195'), true)
   deepStrictEqual(current.get(), { a: 100, c: 0.1, h: 195, l: 0.8 })
 
-  setCurrent('0.8 0.1 195')
+  strictEqual(setCurrent('0.8 0.1 195'), true)
   deepStrictEqual(current.get(), { a: 100, c: 0.1, h: 195, l: 0.8 })
 })
